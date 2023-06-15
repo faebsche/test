fc7036439855b745ac7183ddb04d78006248c984
faebsche <fabian.patterson@rwth-aachen.de>
2023-06-15 12:20:14 +0200

    Update test.py

--- test.py
+++ test.py
@@ -5,8 +5,7 @@ import pandas as pd
 data = pd.read_csv('platten.csv')
 
 # Create a Streamlit map plot
-st.title('Hallo Nömselchen, wo spielen wir bald mal?')
-st.write('mfg nmb')
+
 st.map(data)
 
 '''import streamlit as st
